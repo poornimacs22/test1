2b9f41401d22db42ae6b1075ce152be43da1afa8
poornima <ec2-user@ip-172-31-27-214.eu-north-1.compute.internal>
2025-11-23 18:01:00 +0000

    adding c files

---
+++ big2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void biggestoftwo() {
+    int num1, num2;
+
+    printf("Enter the first number: ");
+    scanf("%d", &num1);
+
+    printf("Enter the second number: ");
+    scanf("%d", &num2);
+
+    if (num1 > num2) {
+        printf("%d is the biggest number.\n", num1);
+    } else if (num2 > num1) {
+        printf("%d is the biggest number.\n", num2);
+    } else {
+        printf("Both numbers are equal.\n");
+    }
+}
+
