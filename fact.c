bfd26ca5d74c89c354026542f8e6d1538be0b811
poornima <ec2-user@ip-172-31-27-214.eu-north-1.compute.internal>
2025-11-23 18:01:26 +0000

    adding factorial

---
+++ fact.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void factorial() {
+    int n, factorial = 1, i;
+
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
+
+    if (n < 0) {
+        printf("Factorial is not defined for negative numbers.\n");
+    } else {
+        for (i = 1; i <= n; i++) {
+            factorial *= i;
+        }
+        printf("Factorial of %d is %d\n", n, factorial);
+    }
+}
+
